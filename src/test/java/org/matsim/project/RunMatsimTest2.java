34b2c298c0af2a13d2d1ce2950d82477f7aea674
theresa <theresa@client-141-23-192-142.wlan.tu-berlin.de>
2019-12-02 15:00:23 +0100

    commit failing test

---
+++ src/test/java/org/matsim/project/RunMatsimTest2.java
@@ -0,0 +1,14 @@
+package org.matsim.project;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class RunMatsimTest2 {
+
+	@Test
+	public void testMain() {
+		fail("Not yet implemented");
+	}
+
+}
